448fed7e17305ce8f299c487798f537bafbd2d36
Svetlana Pedan <lanaksenova@gmail.com>
2026-02-03 10:34:00 +0300

    fixed project after review

--- src/main/java/model/Food.java
+++ src/main/java/model/Food.java
@@ -8,7 +8,7 @@ public abstract class Food implements Discountable {
     protected double price; // цена за единицу (вещественное число)
     protected boolean isVegetarian; // флаг, который показывает, вегетарианский ли продукт
 
-    public Food(int amount, double price, boolean isVegetarian) {
+    protected Food(int amount, double price, boolean isVegetarian) {
         this.amount = amount;
         this.price = price;
         this.isVegetarian = isVegetarian;
